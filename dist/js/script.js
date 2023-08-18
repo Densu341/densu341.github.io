a4a7b1db7a0cef8106ac2d6952eca9da459c3829
DENI IRAWAN <87279587+Densu341@users.noreply.github.com>
2023-08-18 23:08:27 +0700

    upload portfolio with tailwind

---
+++ dist/js/script.js
@@ -0,0 +1,105 @@
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
+
+// Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// Klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
+
+// Formspree
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var alert = document.getElementById("alert-1");
+  // remove style display none
+  alert.style.display = "";
+  var opacity = 0;
+  var interval = setInterval(function () {
+    opacity += 0.1;
+    alert.style.opacity = opacity;
+    if (opacity >= 1) {
+      clearInterval(interval);
+    }
+  }, 200);
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Terimakasih sudah menghubungi saya!";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! Ada yang salah. Coba lagi ya!";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! Ada yang salah. Coba lagi ya!";
+    });
+}
+form.addEventListener("submit", handleSubmit);
